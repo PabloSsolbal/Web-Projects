2d70745c9b3add5f1839ef6f774d897a3d0f26ef
PabloSsolbal <pablossolbal.gmail.com>
2023-07-03 10:54:17 -0600

    weatehr style updated

---
+++ drumKit/script.js
@@ -0,0 +1,27 @@
+const drumPads = document.querySelectorAll(".blue");
+const guitarPads = document.querySelectorAll(".red");
+const pianoPads = document.querySelectorAll(".yellow");
+const synthPads = document.querySelectorAll(".green");
+
+const effectBtns = document.querySelectorAll(".effect");
+
+const drumSounds = [
+  { drum1: "sounds/drum1.wav" },
+  { drum2: "sounds/drum2.wav" },
+  { drum3: "sounds/drum3.wav" },
+];
+const guitarSounds = [
+  { guitar1: "sounds/guitar1.wav" },
+  { guitar2: "sounds/guitar2.wav" },
+  { guitar3: "sounds/guitar3.wav" },
+];
+const pianoSounds = [
+  { piano: "sounds/piano1.wav" },
+  { piano2: "sounds/piano2.wav" },
+  { piano3: "sounds/piano3.wav" },
+];
+const synthSounds = [
+  { synth1: "sounds/synth1.wav" },
+  { synth2: "sounds/synth2.wav" },
+  { synth3: "sounds/synth3.wav" },
+];
